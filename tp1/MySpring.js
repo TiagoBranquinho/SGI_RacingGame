f8f6aec9185ce10fe399fdf74b04a6e49856f0fc
Tiago <t.branquinho.slb@gmail.com>
2023-10-06 10:24:55 +0100

    spring first version

--- tp1/MySpring.js
+++ tp1/MySpring.js
@@ -5,27 +5,50 @@ class MySpring {
 
   constructor(app) {
     this.app = app;
-    this.spring = null;
+    this.springMesh = null;
+    this.springGroup;
     this.springSize = 1;
     this.lastSpringEnabled = null;
     this.springDisplacement = new THREE.Vector3(0, 0, 0);
   }
 
-  buildSpring() {
-    const springCurve = new THREE.CatmullRomCurve3([
-      new THREE.Vector3(0, 0, 0),
-      new THREE.Vector3(1, 1, 1),
-    ]);
+  buildSpringPart() {
+    // Define points for the vertical spring using a Quadratic Bezier curve
+    const startPoint = new THREE.Vector3(0, 0, 0); // Start point
+    const controlPoint = new THREE.Vector3(1, 0.1, 0.5); // Control point to shape the curve
+    const endPoint = new THREE.Vector3(1, 0.2, 0); // End point
+    const curve = new THREE.QuadraticBezierCurve3(startPoint, controlPoint, endPoint);
+    const points = curve.getPoints(100); // Generate points along the curve
 
-    const springGeometry = new THREE.TubeGeometry(springCurve, 200, 0.02, 20, false);
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+    const material = new THREE.LineBasicMaterial({ color: 0x00ff00 });
+    const springMesh = new THREE.Line(geometry, material);
 
-    const springMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    const controlPoint2 = new THREE.Vector3(1, 0.3, -0.5); // Control point to shape the curve
+    const endPoint2 = new THREE.Vector3(0, 0.4, 0); // End point
+    const curve2 = new THREE.QuadraticBezierCurve3(endPoint, controlPoint2, endPoint2);
+    const points2 = curve2.getPoints(100); // Generate points along the curve
 
-    this.spring = new THREE.Mesh(springGeometry, springMaterial);
+    const geometry2 = new THREE.BufferGeometry().setFromPoints(points2);
+    const springPartGroup = new THREE.Group();
+    const springMesh2 = new THREE.Line(geometry2, material);
 
-    this.spring.position.set(0, 5, 0);
+    springPartGroup.add(springMesh);
+    springPartGroup.add(springMesh2);
 
+
+    return springPartGroup;
   }
+
+  buildSpring() {
+    this.springGroup = new THREE.Group();
+    for (let i = 0; i < 5; i++) {
+      const spring = this.buildSpringPart(); // Call your buildSpring() method here
+      spring.position.set(2, 0.4*i, 0); // Adjust the x-coordinate as needed
+      this.springGroup.add(spring); // Add the spring to the array
+    }
+  }
+
 
   init() {
     this.buildSpring();
